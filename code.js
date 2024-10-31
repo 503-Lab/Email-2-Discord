36e8347f3c3560d86a09c6e551642d5c7ff8591c
Marumasa <77374813+malken21@users.noreply.github.com>
2024-10-31 12:38:50 +0900

    for から forEach に置き換え

--- code.js
+++ code.js
@@ -2,25 +2,23 @@ function EmailToDiscord() {
     // 未読のメールを検索
     const threads = GmailApp.search(`is:unread to:${EmailAddress}`);
 
-    if (threads.length > 0) {
-        for (let i = 0; i < threads.length; i++) {
-            const messages = threads[i].getMessages();
-            for (let j = 0; j < messages.length; j++) {
-                const message = messages[j];
-                const subject = message.getSubject();
-                const from = message.getFrom();
+    threads.forEach(thread => {
+        const messages = thread.getMessages();
+        messages.forEach(message => {
 
-                // Discordに送信するメッセージの内容
-                const payload = {
-                    content: `### 新しいメールが届きました\n${from}\n\`\`\`${subject}\`\`\``
-                };
+            const from = message.getFrom();
+            const subject = message.getSubject();
 
-                // Discord WebhookにPOSTリクエストを送信
-                sendDiscord(payload);
+            // Discordに送信するメッセージの内容
+            const payload = {
+                content: `### 新しいメールが届きました\n${from}\n\`\`\`${subject}\`\`\``
+            };
 
-                // メールを既読にする
-                message.markRead();
-            }
-        }
-    }
+            // Discord WebhookにPOSTリクエストを送信
+            sendDiscord(payload);
+
+            // メールを既読にする
+            message.markRead();
+        });
+    });
 }
